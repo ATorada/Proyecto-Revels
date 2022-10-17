9a66ba412f53af9897ef2cc66404a77c65b7e860
AToradaIBIM <atorada@ibim.es>
2022-10-17 14:15:18 +0200

    FEAT - Header half-way

---
+++ includes/cabecera.inc.php
@@ -0,0 +1,17 @@
+<header>
+    <div class="barra_lateral">
+        <h3>Amigos</h3>
+        <a href="#">Amigo 1</a>
+        <a href="#">Amigo 2</a>
+        <a href="#">Amigo 3</a>
+    </div>
+    <div class="cabecera">
+        <a href="index.php" class="logo">Fotico</a>
+        <div class="enlaces">
+            <a class="enlace_resaltado" href="#">Iniciar sesión</a>
+            <a href="#">Nuevo Revel</a>
+            <a href="#">Cuenta</a>
+            <a href="#">Cerrar sesión</a>
+        </div>
+    </div>
+</header>
