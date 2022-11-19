cd479745e9556bb244ac845b7a88ddbc1cf34165
AToradaIBIM <atorada@ibim.es>
2022-11-19 23:34:29 +0100

    STYLE - BackEnd style

--- cancel.php
+++ cancel.php
@@ -43,6 +43,7 @@ if (count($_POST) > 0) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Eliminar cuenta</title>
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/backend.css">
 </head>
 <body>
     <?php
--- list.php
+++ list.php
@@ -15,6 +15,7 @@ if (!isset($_SESSION['usuario'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Revelaciones</title>
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/backend.css">
 </head>
 <body>
     <?php
