e4c2b804007e2cc83077e8bcf69ec86d0540264a
Sabiin <sabiintg@gmail.com>
2022-11-26 00:44:01 +0100

    FINISHED - List.php Finished

--- list.php
+++ list.php
@@ -1,4 +1,12 @@
 <?php
+/*
+    Desarrollado por: Ángel Torada
+
+    Este archivo:
+    - Contiene un listado de las revels del usuario junto a un botón para borrarlo
+*/
+
+//Se comprueba que el usuario esté logueado y se importa la función de conexión a la base de datos
 session_start();
 require_once('includes/conexion.inc.php');
 
@@ -9,6 +17,7 @@ if (!isset($_SESSION['usuario'])) {
 ?>
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -17,28 +26,36 @@ if (!isset($_SESSION['usuario'])) {
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/backend.css">
 </head>
+
 <body>
     <?php
     require_once('includes/cabecera.inc.php');
     echo '<div class="listarRevelaciones">';
     echo '<h1>Tus revelaciones</h1>';
     echo '<div class="revelaciones">';
-    $consulta = $conexion->prepare("SELECT * FROM revels WHERE userid = ?");
-    $consulta->execute([$_SESSION['usuario_id']]);
-    if($consulta->rowCount()>0){
-        while ($revel = $consulta->fetch(PDO::FETCH_ASSOC)) {
-            echo '<div class="revel">';
-            echo '<h2><a href="revel.php?id='.$revel['id'].'">' . $revel['texto'] . '</a></h2>';
-            echo '<p><span class="resaltado">' . $revel['fecha'] . '</span></p>';
-            echo '<a href="delete.php?id=' . $revel['id'] . '">Eliminar</a>';
-            echo '</div>';
+    //Se muestan las revels del usuario
+    try {
+        $conexion = conectar();
+        $consulta = $conexion->prepare("SELECT * FROM revels WHERE userid = ?");
+        $consulta->execute([$_SESSION['usuario_id']]);
+        if ($consulta->rowCount() > 0) {
+            while ($revel = $consulta->fetch(PDO::FETCH_ASSOC)) {
+                echo '<div class="revel">';
+                echo '<h2><a href="revel.php?id=' . $revel['id'] . '">' . $revel['texto'] . '</a></h2>';
+                echo '<p><span class="resaltado">' . $revel['fecha'] . '</span></p>';
+                echo '<a href="delete.php?id=' . $revel['id'] . '">Eliminar</a>';
+                echo '</div>';
+            }
+        } else {
+            echo '<p>No tienes revelaciones</p>';
         }
-    } else {
-        echo '<p>No tienes revelaciones</p>';
+    } catch (\Throwable $th) {
+        echo '<p class="error">Algo ha salido mal</p>';
     }
     echo '</div>';
     echo '</div>';
     ?>
 
 </body>
+
 </html>
