3ab16a0c59b1af9452ae4eba6ae2fec643fd4ee9
Sabiin <sabiintg@gmail.com>
2022-11-25 23:49:02 +0100

    FINISHED - Conexion.inc.php Finished

--- includes/conexion.inc.php
+++ includes/conexion.inc.php
@@ -1,7 +1,13 @@
 <?php
+/*
+    Desarrollado por: Ángel Torada
+
+    Este archivo:
+    - Contiene la función de conexión a la base de datos
+*/
 function conectar()
 {
-    //Se prepara la conexión
+    //Se prepara y devuelve la conexión y en caso de error se devuelve null
     $dsn = 'mysql:host=localhost;dbname=revels';
     $opciones = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
 
