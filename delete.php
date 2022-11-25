b1b73c127920c3a188eba019e8bd7bb0cb83fdd3
Sabiin <sabiintg@gmail.com>
2022-11-26 00:09:51 +0100

    FINISHED - Delete.php Finished

--- delete.php
+++ delete.php
@@ -1,4 +1,12 @@
 <?php
+/*
+    Desarrollado por: Ángel Torada
+
+    Este archivo:
+    - Contiene la lógica para eliminar un revel
+*/
+
+//Se comprueba que el usuario esté logueado y se importa la función de conexión a la base de datos
 session_start();
 require_once('includes/conexion.inc.php');
 
@@ -13,12 +21,15 @@ if ($_GET['id']) {
     $consulta = $conexion->prepare("SELECT * FROM revels WHERE id = ? AND userid = ?");
     $consulta->execute([$_GET['id'], $_SESSION['usuario_id']]);
     if ($consulta->rowCount() > 0) {
+        //Borra los comentarios del revel
         $consulta = $conexion->prepare("DELETE FROM comments WHERE revelid = ?;");
         $consulta->execute([$_GET['id']]);
+        //Borra el revel
         $consulta = $conexion->prepare("DELETE FROM revels WHERE id = ?;");
         $consulta->execute([$_GET['id']]);
         header('Location: list.php');
     }
+    //Se cierra la conexión
     unset($consulta);
     unset($conexion);
 } else {
