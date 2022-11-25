6acdaefec4a4ade2d2f129f2fada16432cf0d783
ATorada <sabiintg@gmail.com>
2022-11-25 16:04:13 +0100

    FIX - Now you're not into result

--- results.php
+++ results.php
@@ -9,12 +9,12 @@ if (!isset($_SESSION['usuario'])) {
 if (count($_POST) > 0) {
     $conexion = conectar();
     if (!is_null($conexion)) {
-        if(isset($_POST['amigo_id_seguir'])) {
+        if (isset($_POST['amigo_id_seguir'])) {
             $consulta = $conexion->prepare('INSERT INTO follows (userid, userfollowed) VALUE (?, ?);');
             $consulta->bindParam(1, $_SESSION['usuario_id']);
             $consulta->bindParam(2, $_POST['amigo_id_seguir']);
             $consulta->execute();
-        } else  {
+        } else {
             $consulta = $conexion->prepare('DELETE FROM follows WHERE userid = ? AND userfollowed = ?;');
             $consulta->bindParam(1, $_SESSION['usuario_id']);
             $consulta->bindParam(2, $_POST['amigo_id_borrar']);
@@ -40,12 +40,13 @@ if (count($_POST) > 0) {
     require_once('includes/cabecera.inc.php');
     echo "<div class='resultados'>";
     echo '<h1>Resultados</h1>';
-    //Muestra los usuarios que coinciden con la búsqueda junto a un botón de añadir
+
 
     if (isset($_GET['busqueda'])) {
         $conexion = conectar();
-        $consulta = $conexion->prepare("SELECT id, usuario FROM users WHERE usuario LIKE ?");
-        $consulta->execute(array('%'.$_GET['busqueda'].'%'));
+        //Muestra los usuarios que coinciden con la búsqueda junto a un botón de añadir sin incluir el usuario actual
+        $consulta = $conexion->prepare("SELECT * FROM users WHERE usuario LIKE ? AND id != ?");
+        $consulta->execute(array('%' . $_GET['busqueda'] . '%', $_SESSION['usuario_id']));
         $resultado = $consulta->fetchAll(PDO::FETCH_ASSOC);
         if ($resultado) {
             foreach ($resultado as $usuario) {
@@ -54,13 +55,13 @@ if (count($_POST) > 0) {
                 $consulta->execute(array($_SESSION['usuario_id'], $usuario['id']));
                 $sigue = $consulta->fetch(PDO::FETCH_ASSOC);
                 echo '<div class="usuario">';
-                echo "<p>".$usuario['usuario']."</p>";
+                echo "<p>" . $usuario['usuario'] . "</p>";
                 echo "<form action='#' method='post'>";
                 if ($sigue) {
-                    echo "<input type='hidden' name='amigo_id_borrar' value='".$usuario['id']."'>";
+                    echo "<input type='hidden' name='amigo_id_borrar' value='" . $usuario['id'] . "'>";
                     echo "<input class='boton borrar_cuenta' type='submit' value='Dejar de seguir'>";
                 } else {
-                    echo "<input type='hidden' name='amigo_id_seguir' value='".$usuario['id']."'>";
+                    echo "<input type='hidden' name='amigo_id_seguir' value='" . $usuario['id'] . "'>";
                     echo "<input  class='boton' type='submit' value='Seguir'>";
                 }
                 echo "</form>";
