1043127df3b2fce3fade40a52329393c23cec314
Sabiin <sabiintg@gmail.com>
2022-11-26 00:06:59 +0100

    FINISHED - Comment.php Finished

--- comment.php
+++ comment.php
@@ -1,17 +1,25 @@
 <?php
+/*
+    Desarrollado por: Ángel Torada
+
+    Este archivo:
+    - Comprueba que el comentario recibido esté correcto, en caso de no estarlo muestra un error
+*/
+
+//Se comprueba que el usuario esté logueado y se importa la función de conexión a la base de datos
 session_start();
 require_once('includes/conexion.inc.php');
 
-if(!isset($_SESSION["usuario"])){
+if (!isset($_SESSION["usuario"])) {
     header("Location: index.php");
 }
 
 //Recibe un comentario por POST y lo guarda en la base de datos si está correcto
 
-if(count($_POST) > 0) {
+if (count($_POST) > 0) {
     $errores = null;
 
-    //Comprueba todos los datos para que ninguno esté vacío
+    //Comprueba que el comentario no esté vacío
     foreach ($_POST as $clave => $valor) {
         $valor = trim($valor);
 
@@ -20,29 +28,33 @@ if(count($_POST) > 0) {
         }
     }
 
+    //Comprueba que el comentario no sea demasiado largo ni demasiado corto
     if ((strlen($_POST["comentario"]) < 20 || strlen($_POST["comentario"]) > 255) && !isset($errores["comentario"])) {
         $errores["texto"] = '<p class="error">El comentario solo recibe de 20 a 255 caracteres.</p><br>';
     }
 
-
+    //Se añade el comentario a la base de datos si no hay errores 
     if (!$errores) {
         $conexion = conectar();
 
         if (!is_null($conexion)) {
-            $consulta = $conexion->prepare('INSERT INTO comments (revelid, userid, texto, fecha) VALUES (?, ?, ?, ?); ');
-            
-            $fecha = date("Y-m-d H:i:s");
-            $consulta->bindParam(1, $_POST["id"]);
-            $consulta->bindParam(2, $_SESSION["usuario_id"]);
-            $consulta->bindParam(3, $_POST["comentario"]);
-            $consulta->bindParam(4, $fecha);
-
             try {
+                $consulta = $conexion->prepare('INSERT INTO comments (revelid, userid, texto, fecha) VALUES (?, ?, ?, ?); ');
+
+                $fecha = date("Y-m-d H:i:s");
+                $consulta->bindParam(1, $_POST["id"]);
+                $consulta->bindParam(2, $_SESSION["usuario_id"]);
+                $consulta->bindParam(3, $_POST["comentario"]);
+                $consulta->bindParam(4, $fecha);
+
                 $consulta->execute();
                 header('Location: revel.php?id=' . $_POST["id"]);
             } catch (PDOException $e) {
             }
         }
+        //Se cierra la conexión
+        unset($conexion);
+        unset($consulta);
     }
 }
 
@@ -50,6 +62,7 @@ if(count($_POST) > 0) {
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -57,19 +70,23 @@ if(count($_POST) > 0) {
     <title>Comment</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
     <?php
+    //Importa el header
     require_once('includes/cabecera.inc.php');
     echo '<div class="errores">';
-
+    //Muestra los errores
     if (isset($errores)) {
         foreach ($errores as $error) {
             echo $error;
         }
     }
+    //Muestra un enlace a la página anterior
     echo '<a href="revel.php?id=' . $_POST["id"] . '">Volver</a>';
     echo '</div>';
     ?>
 
 </body>
+
 </html>
